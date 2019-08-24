7c22de2933c406a79befc39e3c25bd498edfe5a4
visual_community@outlook.com <visual_community@outlook.com>
2019-08-24 11:05:33 -0400

    Solucion Final

---
+++ MVC_Address/MVC_Address/Models/Book.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace MVC_Address.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum TypeContact    
+    {
+        Telephone,
+        Email,
+        Facebook,
+        Twitter,
+        Instagram,
+        dblink
+    }
+
+    public class Book
+    {
+        [Key]
+        public int bookID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public TypeContact type { get; set; }
+        [Required]
+        public string contact { get; set; }
+
+    }
+}
---
+++ MVC_Address/MVC_Address/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace MVC_Address.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+            
+        }
+
+        public System.Data.Entity.DbSet<MVC_Address.Models.Book> Books { get; set; }
+    }
+}
