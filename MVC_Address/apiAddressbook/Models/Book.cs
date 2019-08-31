8b58f26123628a894f87d6533354bb8dba467668
Jose Gutierrez <visual_community@outlook.com>
2019-08-31 09:51:29 -0400

    web api ok

---
+++ MVC_Address/apiAddressbook/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿
+namespace apiAddressbook.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum TypeContact
+    {
+        Telephone,
+        Email,
+        Facebook,
+        Twitter,
+        Instagram,
+        dblink
+    }
+
+    public class Book
+    {
+        [Key]
+        public int bookID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public TypeContact type { get; set; }
+        [Required]
+        public string contact { get; set; }
+
+    }
+}
